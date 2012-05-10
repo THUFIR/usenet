9228ee3548a7fbc64bfd52ddab1905080c45fc90
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-05-10 06:04:13 -0700

    increment

--- src/net/bounceme/dur/usenet/controller/Persist.java
+++ src/net/bounceme/dur/usenet/controller/Persist.java
@@ -9,30 +9,33 @@ import javax.persistence.Query;
 import net.bounceme.dur.usenet.swing.Msg;
 
 public enum Persist {
-
+    
     INSTANCE;
     private final Logger LOG = Logger.getLogger(Persist.class.getName());
     private EntityManagerFactory emf;
     private EntityManager em;
     private String PERSISTENCE_UNIT_NAME = "USENETPU";
-
+    
     Persist() {
         emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         em = emf.createEntityManager();
         LOG.fine("entity manager made???" + em.isOpen());
         populateList();
     }
-
+    
     private void populateList() {
         LOG.fine("open?" + em.isOpen());
         em.getTransaction().begin();
         LOG.fine("trying to populate.....");
-        Query q = em.createNamedQuery("Groups.findAll");
+        Query q = em.createNamedQuery("Notes.getByIdAndGroup");
         LOG.fine(q.toString());
         List results = q.getResultList();
+        for (Object o : results) {
+            LOG.warning("object is \n\n" + o);
+        }
         em.getTransaction().commit();
     }
-
+    
     public void get(Msg message) {
         
     }
