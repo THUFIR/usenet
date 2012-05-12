6514a0b164d1195f8d82a84863c232524520fd80
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-05-11 21:42:28 -0700

    increment

--- src/net/bounceme/dur/usenet/swing/view/NotesTableModel.java
+++ src/net/bounceme/dur/usenet/swing/view/NotesTableModel.java
@@ -2,24 +2,29 @@ package net.bounceme.dur.usenet.swing.view;
 
 import java.util.List;
 import java.util.Vector;
+import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 import net.bounceme.dur.usenet.controller.Notes;
 
 class NotesTableModel extends DefaultTableModel {
 
+    private final static Logger LOG = Logger.getLogger(NotesTableModel.class.getName());
+
     public NotesTableModel() {
     }
 
     @SuppressWarnings("unchecked")
-    public NotesTableModel(List<Notes> notes,MessageBean messageBean) {
+    public NotesTableModel(List<Notes> notes, MessageBean messageBean) {
         addColumn("group");
         addColumn("subject");
         addColumn("note");
-        Vector v = new Vector();
-        for(Notes n : notes){
+        for (Notes n : notes) {
+            Vector v = new Vector();
             v.add(n.getNewsGroup());
             v.add(messageBean.getSubject());
             v.add(n.getNote());
+            this.addRow(v);
         }
+        LOG.warning(notes.toString());
     }
 }
