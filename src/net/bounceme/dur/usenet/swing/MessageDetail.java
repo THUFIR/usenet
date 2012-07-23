0e229fda8587c27c4cd84ddc4dedee2ac3fa6dd4
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-07-23 01:49:48 -0700

    increment

--- src/net/bounceme/dur/usenet/swing/MessageDetail.java
+++ src/net/bounceme/dur/usenet/swing/MessageDetail.java
@@ -26,8 +26,33 @@ public class MessageDetail extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setLayout(new java.awt.BorderLayout());
+        groupSelect1 = new net.bounceme.dur.usenet.swing.GroupSelect();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextPane1 = new javax.swing.JTextPane();
+
+        setLayout(new java.awt.GridLayout(1, 0));
+        add(groupSelect1);
+
+        jList1.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane2.setViewportView(jList1);
+
+        add(jScrollPane2);
+
+        jScrollPane1.setViewportView(jTextPane1);
+
+        add(jScrollPane1);
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private net.bounceme.dur.usenet.swing.GroupSelect groupSelect1;
+    private javax.swing.JList jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
 }
