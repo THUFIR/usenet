496ee5aff1fd1c5a31c580c9c64fe29889bd532c
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-07-23 23:41:24 -0700

    increment

--- src/net/bounceme/dur/usenet/controller/Controller.java
+++ src/net/bounceme/dur/usenet/controller/Controller.java
@@ -3,7 +3,6 @@ package net.bounceme.dur.usenet.controller;
 import java.util.Observable;
 import java.util.logging.Logger;
 import javax.swing.ListModel;
-import net.bounceme.dur.usenet.model.Usenet;
 
 public class Controller extends Observable {
 
---
+++ src/net/bounceme/dur/usenet/controller/MessageBean.java
@@ -0,0 +1,36 @@
+package net.bounceme.dur.usenet.controller;
+
+
+public class MessageBean {
+    private String group;
+    private String title;
+    private String content;
+    
+    public MessageBean(){}
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+    
+}
