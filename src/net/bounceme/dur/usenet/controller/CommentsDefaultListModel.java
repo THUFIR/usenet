91325ed52f6be2b26c7f6456334059bd3a1debfb
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-07-26 06:30:29 -0700

    mysql> mysql> use nntp; Reading table information for completion of table and column names You can turn off this feature to get a quicker startup with -A
    Database changed
    mysql> show tables;
    +----------------+
    | Tables_in_nntp |
    +----------------+
    | comments       |
    | messages       |
    +----------------+
    2 rows in set (0.00 sec)
    
    mysql>
    mysql>
    mysql> ALTER TABLE comments ADD FOREIGN KEY (message_id) REFERENCES messages(id);
    Query OK, 0 rows affected (0.69 sec)
    Records: 0  Duplicates: 0  Warnings: 0
    
    mysql>
    mysql> describe comments;
    +------------+---------+------+-----+---------+-------+
    | Field      | Type    | Null | Key | Default | Extra |
    +------------+---------+------+-----+---------+-------+
    | id         | int(11) | NO   | PRI | NULL    |       |
    | message_id | int(11) | NO   | MUL | NULL    |       |
    | comment    | text    | NO   |     | NULL    |       |
    | stamp      | date    | NO   |     | NULL    |       |
    +------------+---------+------+-----+---------+-------+
    4 rows in set (0.00 sec)
    
    mysql>
    mysql> describe messages;
    +-----------+---------+------+-----+---------+-------+
    | Field     | Type    | Null | Key | Default | Extra |
    +-----------+---------+------+-----+---------+-------+
    | id        | int(11) | NO   | PRI | NULL    |       |
    | newsgroup | text    | NO   |     | NULL    |       |
    | subject   | text    | NO   |     | NULL    |       |
    | content   | text    | NO   |     | NULL    |       |
    | number    | text    | NO   |     | NULL    |       |
    +-----------+---------+------+-----+---------+-------+
    5 rows in set (0.00 sec)
    
    mysql> quit;
    Bye
    thufir@dur:~/NetBeansProjects/USENET$

--- src/net/bounceme/dur/usenet/controller/CommentsDefaultListModel.java
+++ src/net/bounceme/dur/usenet/controller/CommentsDefaultListModel.java
@@ -4,6 +4,24 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
 
+/*
+ * public void persist(Msg message) {
+    LOG.info("\t" + message);
+    LOG.info("isOpen?" + em.isOpen());
+    em.getTransaction().begin();
+    int id = message.getId();
+    Query q = em.createQuery("SELECT  n "
+            + "FROM Notes n WHERE n.messageId = :messageId "
+            + "AND n.group = :group");
+    List results = q.getResultList();
+    em.getTransaction().commit();
+    for (Object o : results) {
+        LOG.info("object is \n\n" + o);
+    }
+}
+
+}
+ */
 public class CommentsDefaultListModel extends DefaultListModel {
 
     private static final Logger LOG = Logger.getLogger(CommentsDefaultListModel.class.getName());
