5ac861ea4e5461dd41760bd5578c40510f8840e8
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-07-26 06:46:35 -0700

    increment

--- src/net/bounceme/dur/usenet/controller/CommentsDefaultListModel.java
+++ src/net/bounceme/dur/usenet/controller/CommentsDefaultListModel.java
@@ -2,29 +2,26 @@ package net.bounceme.dur.usenet.controller;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.swing.DefaultListModel;
 
 /*
- * public void persist(Msg message) {
-    LOG.info("\t" + message);
-    LOG.info("isOpen?" + em.isOpen());
-    em.getTransaction().begin();
-    int id = message.getId();
-    Query q = em.createQuery("SELECT  n "
-            + "FROM Notes n WHERE n.messageId = :messageId "
-            + "AND n.group = :group");
-    List results = q.getResultList();
-    em.getTransaction().commit();
-    for (Object o : results) {
-        LOG.info("object is \n\n" + o);
-    }
-}
-
-}
+ * public void persist(Msg message) { LOG.info("\t" + message);
+ * LOG.info("isOpen?" + em.isOpen()); em.getTransaction().begin(); int id =
+ * message.getId(); Query q = em.createQuery("SELECT n " + "FROM Notes n WHERE
+ * n.messageId = :messageId " + "AND n.group = :group"); List results =
+ * q.getResultList(); em.getTransaction().commit(); for (Object o : results) {
+ * LOG.info("object is \n\n" + o); } }
+ *
+ * }
  */
 public class CommentsDefaultListModel extends DefaultListModel {
 
     private static final Logger LOG = Logger.getLogger(CommentsDefaultListModel.class.getName());
+    private EntityManagerFactory emf;
+    private EntityManager em;
 
     public CommentsDefaultListModel() {
     }
@@ -36,9 +33,13 @@ public class CommentsDefaultListModel extends DefaultListModel {
             Logger.getLogger(CommentsDefaultListModel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     @SuppressWarnings("unchecked")
     private void load(MessageBean messageBean) throws Exception {
-        LOG.fine("loading.." + messageBean);
+        LOG.severe("loading.." + messageBean);
+
+        emf = Persistence.createEntityManagerFactory("examplePersistenceUnit");
+        em = emf.createEntityManager();
+
     }
 }
