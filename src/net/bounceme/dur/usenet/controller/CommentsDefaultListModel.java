ae763253a9a260820d70727ad424219db1dc0e72
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-07-26 05:43:40 -0700

    increment

--- src/net/bounceme/dur/usenet/controller/CommentsDefaultListModel.java
+++ src/net/bounceme/dur/usenet/controller/CommentsDefaultListModel.java
@@ -1,17 +1,12 @@
 package net.bounceme.dur.usenet.controller;
 
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.mail.Folder;
-import javax.mail.Message;
 import javax.swing.DefaultListModel;
-import net.bounceme.dur.usenet.model.Usenet;
 
 public class CommentsDefaultListModel extends DefaultListModel {
 
     private static final Logger LOG = Logger.getLogger(CommentsDefaultListModel.class.getName());
-    private Usenet usenet = Usenet.INSTANCE;
 
     public CommentsDefaultListModel() {
     }
@@ -23,7 +18,6 @@ public class CommentsDefaultListModel extends DefaultListModel {
             Logger.getLogger(CommentsDefaultListModel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
     
     @SuppressWarnings("unchecked")
     private void load(MessageBean messageBean) throws Exception {
