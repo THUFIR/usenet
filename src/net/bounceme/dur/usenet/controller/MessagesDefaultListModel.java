05e55fd1e150f8be88d1c936cde25e2d6983d88c
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-07-27 22:20:59 -0700

    increment

--- src/net/bounceme/dur/usenet/controller/MessagesDefaultListModel.java
+++ src/net/bounceme/dur/usenet/controller/MessagesDefaultListModel.java
@@ -5,13 +5,19 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.mail.Folder;
 import javax.mail.Message;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.swing.DefaultListModel;
+import net.bounceme.dur.usenet.model.Article;
 import net.bounceme.dur.usenet.model.Usenet;
 
 public class MessagesDefaultListModel extends DefaultListModel {
 
     private static final Logger LOG = Logger.getLogger(MessagesDefaultListModel.class.getName());
     private Usenet usenet = Usenet.INSTANCE;
+    private EntityManagerFactory emf;
+    private EntityManager em;
 
     public MessagesDefaultListModel() {
     }
@@ -24,7 +30,6 @@ public class MessagesDefaultListModel extends DefaultListModel {
         }
     }
 
-    
     @SuppressWarnings("unchecked")
     private void load(Folder folder) throws Exception {
         LOG.fine("loading.." + folder);
@@ -36,5 +41,18 @@ public class MessagesDefaultListModel extends DefaultListModel {
             LOG.fine(messageBean.toString());
         }
         LOG.fine("..loaded");
+    }
+
+    private void persist(MessageBean messageBean) throws Exception {
+        LOG.info("insert to database.." + messageBean);
+
+        Article article = new Article(messageBean);
+
+        emf = Persistence.createEntityManagerFactory("USENETPU");
+        em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(article);
+        em.getTransaction().commit();
     }
 }
