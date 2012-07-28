717f5cf3403c1d910756a1dbb4e1cb310a4c1b4d
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-07-27 19:37:19 -0700

    increment

---
+++ src/net/bounceme/dur/usenet/model/Comment.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.bounceme.dur.usenet.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author thufir
+ */
+@Entity
+@Table(name = "comment", catalog = "nntp", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Comment.findAll", query = "SELECT c FROM Comment c"),
+    @NamedQuery(name = "Comment.findById", query = "SELECT c FROM Comment c WHERE c.id = :id"),
+    @NamedQuery(name = "Comment.findByStamp", query = "SELECT c FROM Comment c WHERE c.stamp = :stamp")})
+public class Comment implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "comment", nullable = false, length = 65535)
+    private String comment;
+    @Basic(optional = false)
+    @Column(name = "stamp", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date stamp;
+    @JoinColumn(name = "message_id", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false)
+    private Message messageId;
+
+    public Comment() {
+    }
+
+    public Comment(Integer id) {
+        this.id = id;
+    }
+
+    public Comment(Integer id, String comment, Date stamp) {
+        this.id = id;
+        this.comment = comment;
+        this.stamp = stamp;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Date getStamp() {
+        return stamp;
+    }
+
+    public void setStamp(Date stamp) {
+        this.stamp = stamp;
+    }
+
+    public Message getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Message messageId) {
+        this.messageId = messageId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Comment)) {
+            return false;
+        }
+        Comment other = (Comment) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "net.bounceme.dur.usenet.model.Comment[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/net/bounceme/dur/usenet/model/Message.java
@@ -0,0 +1,138 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.bounceme.dur.usenet.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author thufir
+ */
+@Entity
+@Table(name = "message", catalog = "nntp", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Message.findAll", query = "SELECT m FROM Message m"),
+    @NamedQuery(name = "Message.findById", query = "SELECT m FROM Message m WHERE m.id = :id")})
+public class Message implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "newsgroup", nullable = false, length = 65535)
+    private String newsgroup;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "subject", nullable = false, length = 65535)
+    private String subject;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "content", nullable = false, length = 65535)
+    private String content;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "number", nullable = false, length = 65535)
+    private String number;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "messageId")
+    private Collection<Comment> commentCollection;
+
+    public Message() {
+    }
+
+    public Message(Integer id) {
+        this.id = id;
+    }
+
+    public Message(Integer id, String newsgroup, String subject, String content, String number) {
+        this.id = id;
+        this.newsgroup = newsgroup;
+        this.subject = subject;
+        this.content = content;
+        this.number = number;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNewsgroup() {
+        return newsgroup;
+    }
+
+    public void setNewsgroup(String newsgroup) {
+        this.newsgroup = newsgroup;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @XmlTransient
+    public Collection<Comment> getCommentCollection() {
+        return commentCollection;
+    }
+
+    public void setCommentCollection(Collection<Comment> commentCollection) {
+        this.commentCollection = commentCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Message)) {
+            return false;
+        }
+        Message other = (Message) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "net.bounceme.dur.usenet.model.Message[ id=" + id + " ]";
+    }
+    
+}
