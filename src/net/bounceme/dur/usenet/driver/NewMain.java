30236a1cfd93f5af38ce1b165edb72ec31e4ad4f
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-07-29 06:26:10 -0700

    increment

---
+++ src/net/bounceme/dur/usenet/driver/NewMain.java
@@ -0,0 +1,43 @@
+package net.bounceme.dur.usenet.driver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.mail.Folder;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import net.bounceme.dur.usenet.model.Newsgroups;
+import net.bounceme.dur.usenet.model.Usenet;
+
+public class NewMain {
+
+    private static final Logger LOG = Logger.getLogger(NewMain.class.getName());
+    private Usenet u = Usenet.INSTANCE;
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    public static void main(String[] args) {
+
+        NewMain nm = new NewMain();
+    }
+
+    public NewMain() {
+        folders();
+    }
+
+    private void folders() {
+        List<Folder> folders = u.getFolders();
+        List<Newsgroups> ng = new ArrayList<>();
+        emf = Persistence.createEntityManagerFactory("USENETPU");
+        em = emf.createEntityManager();
+        for (Folder folder : folders) {
+            Newsgroups n = new Newsgroups(folder);
+            ng.add(n);
+            em.getTransaction().begin();
+            em.persist(n);
+            em.getTransaction().commit();
+        }
+        em.close();
+    }
+}
---
+++ src/net/bounceme/dur/usenet/model/Newsgroups.java
@@ -0,0 +1,64 @@
+package net.bounceme.dur.usenet.model;
+
+import java.io.Serializable;
+import javax.mail.Folder;
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "newsgroups", catalog = "usenet", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Newsgroups.findAll", query = "SELECT n FROM Newsgroups n"),
+    @NamedQuery(name = "Newsgroups.findById", query = "SELECT n FROM Newsgroups n WHERE n.id = :id")})
+public class Newsgroups implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column
+    private String newsgroup;
+
+    public Newsgroups() {
+    }
+
+    public Newsgroups(Folder folder) {
+        newsgroup = folder.getFullName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Newsgroups)) {
+            return false;
+        }
+        Newsgroups other = (Newsgroups) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "net.bounceme.dur.usenet.model.Newsgroups[ id=" + id + " ]";
+    }
+}
